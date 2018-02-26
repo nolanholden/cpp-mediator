c4f9ab3baa1149b7bb44edf3a26e7570fecd20b4
Nolan Holden <nolanholden@users.noreply.github.com>
2018-02-26 05:33:36 -0500

    add more tests for asserting proper resolution this includes throwing std::invalid_argument on unregistered handler requests

--- tests/mediator_unittests.cc
+++ tests/mediator_unittests.cc
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <stdexcept>
 
 
 #define USE(x) do { (void)x; } while (0)
@@ -16,7 +17,7 @@
 // typenames resolve as:
 //         request:  "Req[id]"
 // request_handler:  "ReqHandler[id]"
-#define DefReqHanCustomRequest( \
+#define DEF_REQ_HAN_CUSTOM_REQUEST( \
   id, \
   return_type, \
   request_param_name, \
@@ -39,12 +40,12 @@
     } \
   }
 
-#define DefReqHan( \
+#define DEF_REQ_HAN( \
   id, \
   return_type, \
   request_param_name, \
   handle_code) \
-  DefReqHanCustomRequest( \
+  DEF_REQ_HAN_CUSTOM_REQUEST( \
     id, \
     return_type, \
     request_param_name, \
@@ -54,43 +55,93 @@
 
 
 
+#define M() holden::mediator m{}
+#define REG_HAN(id) \
+  auto handler##id = std::make_shared<ReqHandler##id>(); \
+  m.register_handler(handler##id)
 #define SendForWithArgs(id, request_args) \
-  holden::mediator m{}; \
-  auto handler = std::make_shared<ReqHandler##id>(); \
-  m.register_handler(handler); \
-  Req##id request{request_args}; \
-  auto actual = m.send(request); \
+  M(); \
+  REG_HAN(id); \
+  Req##id request_##id{request_args}; \
+  auto actual = m.send(request_##id)
 
-#define SendFor(id) SendForWithArgs(id, /* no args */);
+#define SendFor(id) SendForWithArgs(id, /* no args */)
 
 
 
 
 // TESTS BELOW:
 
-DefReqHan(a, int, m, return 127;);
-TEST(cpp_mediator, send_recv1) {
+DEF_REQ_HAN(a, int, m, return 127;);
+TEST(cpp_mediator, send_receive1) {
   SendFor(a);
   ASSERT_EQ(127, actual);
 }
 
-DefReqHanCustomRequest(b, int, m, return m.x;, int x;, (int _x) : x(_x) {});
-TEST(cpp_mediator, send_recv2) {
+DEF_REQ_HAN_CUSTOM_REQUEST(b, int, m, return m.x;, int x;, (int _x) : x(_x) {});
+TEST(cpp_mediator, send_receive2) {
   SendForWithArgs(b, 1000);
   ASSERT_EQ(1000, actual);
 }
 
-DefReqHanCustomRequest(c, int, m, return m.x + 1;, int x;, (int _x) : x(_x) {});
-TEST(cpp_mediator, send_recv3) {
+DEF_REQ_HAN_CUSTOM_REQUEST(c, int, m, return m.x + 1;, int x;, (int _x) : x(_x){});
+TEST(cpp_mediator, send_receive3) {
   SendForWithArgs(c, 1000);
   ASSERT_EQ(1001, actual);
 }
 
-DefReqHanCustomRequest(d, int, m, return (*m.x)++;, int* x;, (int* _x) : x(_x) {});
-TEST(cpp_mediator, send_recv4) {
+DEF_REQ_HAN_CUSTOM_REQUEST(d, int, m, return (*m.x)++;, int* x;, (int* _x) : x(_x){});
+TEST(cpp_mediator, request_mutations_preserved) {
   int number = 100;
   SendForWithArgs(d, &number);
   ASSERT_EQ(100, actual);
-  ASSERT_EQ(101, *request.x);
+  ASSERT_EQ(101, *request_d.x);
   ASSERT_EQ(101, number);
+  actual = m.send(request_d);
+  ASSERT_EQ(101, actual);
+  ASSERT_EQ(102, *request_d.x);
+  ASSERT_EQ(102, number);
+}
+
+TEST(cpp_mediator, assert_throw_on_no_register) {
+  M();
+  ASSERT_THROW(m.send(Reqa{}), std::invalid_argument);
+}
+
+TEST(cpp_mediator, assert_proper_resolution) {
+  M();
+  Reqa request_a{};
+  Reqb request_b{3};
+  Reqc request_c{3};
+  int three = 3;
+  Reqd request_d{&three};
+
+  ASSERT_THROW(m.send(request_a), std::invalid_argument);
+  ASSERT_THROW(m.send(request_b), std::invalid_argument);
+  ASSERT_THROW(m.send(request_c), std::invalid_argument);
+  ASSERT_THROW(m.send(request_d), std::invalid_argument);
+
+  REG_HAN(a);
+  ASSERT_THROW(m.send(request_b), std::invalid_argument);
+  ASSERT_THROW(m.send(request_c), std::invalid_argument);
+  ASSERT_THROW(m.send(request_d), std::invalid_argument);
+
+  REG_HAN(b);
+  ASSERT_THROW(m.send(request_c), std::invalid_argument);
+  ASSERT_THROW(m.send(request_d), std::invalid_argument);
+
+  REG_HAN(c);
+  ASSERT_THROW(m.send(request_d), std::invalid_argument);
+
+  REG_HAN(d);
+  
+  ASSERT_EQ(127,    m.send(request_a));
+  ASSERT_EQ(3,      m.send(request_b));
+  ASSERT_EQ(3 + 1,  m.send(request_c));
+  ASSERT_EQ(3,      m.send(request_d));
+  ASSERT_EQ(4, *request_d.x);
+  ASSERT_EQ(4, three);
+  ASSERT_EQ(4, m.send(request_d));
+  ASSERT_EQ(5, *request_d.x);
+  ASSERT_EQ(5, three);
 }
